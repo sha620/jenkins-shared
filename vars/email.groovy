5961da784f92e8d86ed0b07ab4b8de05112f42c4
sha620 <shaktivicky91@gmail.com>
2025-08-16 16:40:54 +0530

    Update email.groovy

--- vars/email.groovy
+++ vars/email.groovy
@@ -1,19 +1,9 @@
 def call(Sring from,String toto){
-success{
         script{
             emailext from: "${from}",
             to: "${toto}",
             subject: "pass",
             body: "pass"
         }
-    }
-    failure{
-        script{
-            emailext from: "${from}",
-            to: "${toto}",
-            subject: "fail",
-            body: "fail"
-        }
-    }
   
 }
